0e65dcb9df1110e701461cdcde6204d01769aa08
Joseph Simpson <jjs0sbw@gmail.com>
2014-11-21 23:04:26 -0800

    Working class project CI setup

---
+++ lib/binary_matrix_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe BinaryMatrix do 
+  
+  before :each do
+    @binary_matrix = BinaryMatrix(10)
+  end
+
+  describe "#new" do
+    it "takes one parameter and returns a BinaryMatrix object" do
+      expect(@binary_matrix).to be_a_kind_of(BinaryMatrix)
+    end
+  end  
+end
